c0eb4894f647c564b5d8aa65a83b05e83ab21e35
ArunSahu1992 <31402954+ArunSahu1992@users.noreply.github.com>
2023-01-10 21:11:14 +0530

    Update token.ps1

--- .github/workflows/token.ps1
+++ .github/workflows/token.ps1
@@ -1,3 +1,5 @@
 $token = "arun sahu";
 write-host "test112"
+
+Get-ChildItem -Path "E:\DotNet\Travel\bin" -Filter '*.dll" | ForEach-Object { [Refelction.Assembly]::LoadFile($_.FullName) }
 return $token
